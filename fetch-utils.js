27ceebefe796df646bb5d4f04a882c64e5099baa
Victor Caruso <vic.caruso@gmail.com>
2022-01-07 10:27:38 -0800

    majority of work done. All buttons hooked up and all fetchutils created

--- fetch-utils.js
+++ fetch-utils.js
@@ -3,10 +3,51 @@ const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsI
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+export async function getItems() {
+    const response = await client
+        .from('shopping_list')
+        .select()
+        .order('purchased');
+    
+    return checkError(response);
+}
+
+
+export async function createItem(qty, item) {
+    const response = await client
+        .from('shopping_list')
+        .insert([
+            {
+                qty: qty,
+                item: item,
+                purchased: false
+            }
+        ]);
+    return checkError(response);
+}
+
+export async function buyItem(id) {
+    const response = await client
+        .from('shopping_list')
+        .update({ purchased: true })
+        .match({ id: id });
+    return checkError(response);
+}
+
+export async function deleteAllItems() {
+    const response = await client
+        .from('shopping_list')
+        .delete();
+    return checkError(response);
+}
+
+
+// ################################################################################################################################
+// ###
+// #
 export async function getUser() {
     return client.auth.session();
 }
-
 
 export async function checkAuth() {
     const user = await getUser();
@@ -41,3 +82,6 @@ export async function logout() {
 function checkError({ data, error }) {
     return error ? console.error(error) : data;
 }
+// #
+// ###
+// ################################################################################################################################
--- shopping-list/shopping-list.js
+++ shopping-list/shopping-list.js
@@ -1,9 +1,69 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, logout, getItems, createItem, buyItem, deleteAllItems } from '../fetch-utils.js';
 
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const itemForm = document.querySelector('#item-form');
+const addItemButton = document.querySelector('#add-button');
+const deleteAllButton = document.querySelector('#delete-button');
+const qtyInputEl = document.querySelector('#qty');
+const itemInputEl = document.querySelector('#item-name');
+const listContainerEl = document.querySelector('#list-container');
 
-logoutButton.addEventListener('click', () => {
-    logout();
+window.addEventListener('load', async() => {
+    displayShoppingListItems();
+});
+
+addItemButton.addEventListener('click', async(e) => {
+    e.preventDefault();
+
+    const data = new FormData(itemForm);
+    const qty = data.get('qty');
+    const item = data.get('item-name');
+
+    await createItem(qty, item);
+
+    displayShoppingListItems();
+});
+
+deleteAllButton.addEventListener('click', async() => {
+    await deleteAllItems();
+    displayShoppingListItems();
+});
+
+async function displayShoppingListItems() {
+    const list = await getItems();
+
+    while (listContainerEl.firstChild) {
+        listContainerEl.firstChild.remove();
+    }
+    for (let item of list) {
+        const itemEl = renderItem(item);
+        itemEl.addEventListener('click', async() => {
+            await buyItem(item.id);
+            displayShoppingListItems();
+        });
+        listContainerEl.append(itemEl);
+    }
+}
+
+function renderItem(item) {
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+
+    p.textContent = `${item.qty} ${item.item}`;
+    div.append(p);
+
+    if (item.purchased) {
+        div.classList.add('purchased');
+    } else {
+        div.classList.remove('purchased');
+        div.classList.add('not-purchased');
+    }
+
+    return div;
+}
+
+logoutButton.addEventListener('click', async() => {
+    await logout();
 });
